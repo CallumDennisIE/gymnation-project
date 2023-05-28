d5d1941eb0c0ce83446756adee4759e29ec54e70
CallumDennisIE <callumdennisie@gmail.com>
2023-05-28 18:29:57 +0000

    feat: add message alerts to account authentication

--- gymnation/settings.py
+++ gymnation/settings.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 import os
 import dj_database_url
+from django.contrib.messages import constants as messages
+
 if os.path.isfile('env.py'):
     import env
 
@@ -42,6 +44,14 @@ ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
