d896949a1e5cc5356ac534ef70b501a3f216f67f
CallumDennisIE <callumdennisie@gmail.com>
2023-05-30 20:51:19 +0000

    build: remove debug from project

--- gymnation/settings.py
+++ gymnation/settings.py
@@ -16,7 +16,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-callumdennisie-gymnation-39ayalftu9.us2.codeanyapp.com',
                  'gymnation-project.herokuapp.com', 'localhost']
